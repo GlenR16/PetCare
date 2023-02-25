f6b3fe4c4cb3efe143a60757e7ce07d5a46f2b84
RIDDHI DINESH OZA <87781977+RIDDHIOZA25@users.noreply.github.com>
2023-02-25 19:32:05 +0530

    Add files via upload
    Web Page Files

---
+++ WebSite/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'dmserif': ['DM Serif', 'serif'],
+        'com': ['Comfortaa', 'cursive'],
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
